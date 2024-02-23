d1f975e057df0fc2545431a3077209f497db01d8
Krystian Wojewski <krystian.a.wojewski@gmail.com>
2024-02-23 18:00:42 +0100

    add: old Mnist project

---
+++ classify.py
@@ -0,0 +1,69 @@
+﻿import os
+
+import matplotlib.pyplot as plt
+
+# Disable TF warning messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+import numpy as np
+import cv2     # python -m pip install opencv-python
+from keras.datasets import mnist
+
+# Directory with test set
+TEST_DATASET_DIR = 'mnist-test'
+
+# Trained model filename
+MODEL = 'model.h5'
+
+if __name__ == "__main__":
+    
+    # Load trained model
+    model = tf.keras.models.load_model(MODEL)
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    for i in range (0, len(x_test)):
+        
+        # Load the image
+        # image = cv2.imread(TEST_DATASET_DIR + os.path.sep + image_name, cv2.IMREAD_GRAYSCALE)
+        image = x_test[i]
+
+        # Pre-process the image for classification
+        image_data = image.astype('float32') / 255
+        image_data = tf.keras.preprocessing.image.img_to_array(image_data)
+        # Expand dimension (28,28,1) -> (1,28,28,1)
+        image_data = np.expand_dims(image_data, axis=0)
+        
+        # Classify the input image
+        prediction = model.predict(image_data)
+        
+        # Find the winner class and the probability
+        winner_class = np.argmax(prediction)
+        winner_probability = np.max(prediction)*100
+
+        second_class = np.argsort(np.max(prediction, axis=0))[-2]
+        second_probability = np.unique(prediction)[-2]*100
+
+        third_class = np.argsort(np.max(prediction, axis=0))[-3]
+        third_probability = np.unique(prediction)[-3] * 100
+
+        # Build the text label
+        label = f"prediction = {winner_class} ({winner_probability:.2f}%)"
+        label2 = f"prediction 2 = {second_class} ({second_probability:.2f}%)"
+        label3 = f"prediction 3 = {third_class} ({third_probability:.2f}%)"
+
+        # Draw the label on the image
+        output_image = cv2.resize(image, (500,500))
+        output_image = cv2.putText(output_image, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.75, 255, 2)
+        if (second_probability > 1) :
+            output_image = cv2.putText(output_image, label2, (10, 55), cv2.FONT_HERSHEY_SIMPLEX, 0.75, 255, 2)
+            if (third_probability > 1):
+                output_image = cv2.putText(output_image, label3, (10, 85), cv2.FONT_HERSHEY_SIMPLEX, 0.75, 255, 2)
+
+        # Show the output image        
+        cv2.imshow("Output", output_image)
+        
+        # Break on 'q' pressed, continue on the other key
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            break
---
+++ train.py
@@ -0,0 +1,79 @@
+﻿import os
+# Disable TF warning messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+EPOCHS = 10
+MODEL_FILENAME = "model.h5"
+NO_OF_CLASSES = 10
+VAL_SPLIT = 0.2
+HIDDEN_UNITS = 50
+
+class FullyConnectedForMnist:
+    '''Simple NN for MNIST database. INPUT => FC/RELU => FC/SOFTMAX'''
+    def build(hidden_units):
+        # Initialize the model
+        model = tf.keras.models.Sequential()
+        # Flatten the input data of (x, y, 1) dimension
+        model.add(tf.keras.layers.Flatten(input_shape=(28,28,1)))
+        # FC/RELU layer
+        model.add(tf.keras.layers.Dense(hidden_units, activation='relu'))
+        # Softmax classifier (10 classes)
+        model.add(tf.keras.layers.Dense(NO_OF_CLASSES, activation="softmax"))
+        return model
+
+
+if __name__ == "__main__":
+    
+    # Load dataset as train and test sets
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+    # Convert from uint8 to float32 and normalize to [0,1]
+    x_train = x_train.astype('float32')/255
+    x_test = x_test.astype('float32')/255
+
+    # Transform labels to 'one-hot' encoding, e.g.
+    # 2 -> [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+    # 6 -> [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+    y_train = tf.keras.utils.to_categorical(y_train, NO_OF_CLASSES)
+    y_test = tf.keras.utils.to_categorical(y_test, NO_OF_CLASSES)
+
+    # Construct the model
+    model = FullyConnectedForMnist.build(HIDDEN_UNITS)
+
+    # Compile the model and print summary
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+
+    # Train the model
+    history = model.fit(x=x_train, y=y_train, epochs=EPOCHS, validation_split=VAL_SPLIT)
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    accuracy = history.history['accuracy']
+    val_accuracy = history.history['val_accuracy']
+
+    # Save model to a file
+    model.save(MODEL_FILENAME)
+
+    # Evaluate the model on the test data
+    model.evaluate(x_test, y_test)
+
+    plt.plot(loss, label="loss")
+    plt.plot(val_loss, label="val_loss")
+    plt.legend()
+    plt.title('Loss chart')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.savefig('loss.png')
+    plt.show()
+
+    plt.plot(accuracy, label="accuracy")
+    plt.plot(val_accuracy, label="val_accuracy")
+    plt.legend()
+    plt.title('Accuracy chart')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.savefig('accuracy.png')
+    plt.show()
